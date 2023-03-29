45c1aa0f0a5cf626ee55be2be1d7ef3b93a3ead1
sky0621 <sky14work@gmail.com>
2023-03-30 00:42:13 +0900

    init

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"github.com/signintech/gopdf"
+	"log"
+	"strconv"
+)
+
+//go:embed Ubuntu-L.ttf
+var ttf []byte
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	numOfPages := 1
+	if len(args) > 0 {
+		a0 := args[0]
+		i, err := strconv.Atoi(a0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numOfPages = i
+	}
+
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
+	if err := pdf.AddTTFFontData("Ubuntu-L", ttf); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := pdf.SetFont("Ubuntu-L", "", 24); err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < numOfPages; i++ {
+		pdf.AddPage()
+		pdf.Cell(nil, fmt.Sprintf("A%d", i+1))
+	}
+
+	pdf.WritePdf("sample.pdf")
+}
